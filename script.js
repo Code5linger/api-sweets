5536ba01c587ca298a0db056bf372e56c4cf31eb
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2025-03-30 14:19:45 +0600

    Done!

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
-fetch(`https://api.jsonbin.io/v3/b/67e8f80b8561e97a50f580ff`, {
-  headers: {
-    'Content-Type': 'application/json',
-  },
-})
-  .then((response) => response.json())
+fetch('https://api.jsonbin.io/v3/b/67e8f80b8561e97a50f580ff')
+  .then((res) => res.json())
   .then((data) => {
-    console.log(data.record.sweets);
-  })
-  .catch((error) => console.error('Error fetching data:', error));
+    data.record.sweets.forEach((sweet) => {
+      document.body.innerHTML += `
+        <div style="margin: 20px; text-align: center;">
+          <h3>${sweet.name}</h3>
+          <img src="${sweet.image}" width="200" alt="${sweet.name}" />
+          <p>${sweet.shortDescription}</p>
+        </div>
+      `;
+    });
+  });
+
+//   ! Last one is undefined
